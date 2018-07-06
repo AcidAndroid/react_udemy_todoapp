92ffb53e9d720ab7bf9abe9b1ac4c4caf2395748
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2018-07-06 17:07:35 -0500

    add y delete agregados

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+
+  state={
+    todos:[]
+    ,id:0
+    ,input:''
+  }
+  addTodo = (todoName)=>{
+
+    this.setState(prevState =>{
+      const newTodos = prevState.todos.slice().concat({
+        name:todoName
+        ,id:prevState.id
+      })
+      return{
+        todos:newTodos
+        ,id:prevState.id+1
+      }
+    })
+
+  }
+
+
+  deleteTodo =(todoId)=>{
+    this.setState(prevState =>{
+      const newTodos=prevState.todos.slice().filter( todo =>{
+        return todo.id !==todoId
+      })
+      return{
+        todos:newTodos        
+      }
+    })
+
+  }
+
+
+  saveTextTodo =(e)=>{
+    const text = e.target.value
+    console.log(text)
+
+    this.setState({
+      input:text
+    })
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <input type="text" value={this.state.input} onChange={this.saveTextTodo} ></input>
+        <button onClick={this.addTodo} >Add Todo</button>
+        <ul>
+          {this.state.todos.map((item) => {            
+            return (<li key={item.id} ></li>)
+          })}
+        </ul>
+
       </div>
     );
   }
